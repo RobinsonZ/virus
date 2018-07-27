f41195790d4e6b8e7815ce0f9422f610fc5b5a2a
Zachary Robinson <robinsonz@catlin.edu>
2018-07-27 11:32:06 -0400

    Add coroutines dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import org.jetbrains.kotlin.gradle.dsl.Coroutines
+import org.jetbrains.kotlin.gradle.dsl.Coroutines.ENABLE
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
@@ -14,12 +16,20 @@ repositories {
 
 dependencies {
     compile(kotlin("stdlib-jdk8"))
+    compile("org.jetbrains.kotlinx:kotlinx-coroutines-core:0.24.0")
     compile("com.google.apis:google-api-services-drive:v3-rev124-1.23.0")
+}
+
+kotlin {
+    experimental {
+        coroutines = ENABLE
+    }
 }
 
 configure<JavaPluginConvention> {
     sourceCompatibility = JavaVersion.VERSION_1_8
 }
+
 tasks.withType<KotlinCompile> {
     kotlinOptions.jvmTarget = "1.8"
 }
